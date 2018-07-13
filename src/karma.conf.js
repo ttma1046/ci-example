c42bfdb0959cf56749d0c0805739646ae06d97a6
Chandler Fang <chandler.fang@cashrewards.com>
2018-07-13 17:43:05 +1000

    typo.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
         base: 'ChromeHeadless',
         flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9233', '--no-sandbox']
       }
-    }
+    },
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
